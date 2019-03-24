c91aa5abdbc9dd300fbc21c649e5a646065026fc
dias <daniel.dddl.pdf@gmail.com>
2019-03-24 16:53:12 -0300

    init. file reading

---
+++ frequence-letters/dinamically-sized-array.c
@@ -0,0 +1,66 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "dinamically-sized-array.h"
+
+#define GROWING_RATE 2
+
+/**
+ * TODO encapsulate memory allocation into a separated function
+ * 
+*/
+
+void initArray(Array *a, size_t initialSize)
+{
+    int size = initialSize * sizeof(char *);
+
+    a->array = (char **)malloc(size);
+    for (unsigned int i = 0; i < initialSize; i++)
+    {
+        a->array[i] = (char *)malloc(MAX_NUMBER_CHARACTERS * sizeof(char));
+    }
+
+    a->used = 0;
+    a->size = initialSize;
+}
+
+void insertInto(Array *a, char *element)
+{
+    if (a->used == a->size)
+    {
+        a->size = a->size + GROWING_RATE;
+        int allocSize = a->size * sizeof(char *);
+
+        a->array = (char **)realloc(a->array, allocSize);
+        for (unsigned int i = a->used; i < a->size; i++)
+        {
+            a->array[i] = (char *)malloc(MAX_NUMBER_CHARACTERS * sizeof(char));
+        }
+    }
+
+    strcpy(a->array[a->used], element);
+    a->used++;
+
+    printArray(a);
+}
+
+void freeArray(Array *a)
+{
+    free(a->array);
+    a->array = NULL;
+    a->used = a->size = 0;
+}
+
+void printArray(Array *a)
+{
+
+    printf("\nPrint array. Size: %lu, Used space: %lu\n", a->size, a->used);
+
+    unsigned int i;
+    for (i = 0; i < a->used; i++)
+    {
+        printf("%d = %s\n", i, a->array[i]);
+    }
+}
---
+++ frequence-letters/dinamically-sized-array.h
@@ -0,0 +1,25 @@
+#ifndef DINAMICALLY_SIZED_ARRAY
+#define DINAMICALLY_SIZED_ARRAY
+
+#include <stdlib.h>
+
+#define MAX_NUMBER_CHARACTERS 1000
+
+/**
+ * This is an struct that stores a pointer of pointer
+ * to be navigated using its size, that defines
+ * how much memory was stored for the array values.
+*/
+typedef struct
+{
+    char **array;
+    size_t used;
+    size_t size;
+} Array;
+
+void initArray(Array *a, size_t initialSize);
+void insertInto(Array *a, char *element);
+void freeArray(Array *a);
+void printArray(Array *a);
+
+#endif
