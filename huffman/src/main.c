c2a915aa6599f9738394ed8cdfd0a7ae9f694c13
DanielDDDL <daniel.dddl.pdf@gmail.com>
2019-10-29 18:30:23 -0300

    setting up environment to develop second PE

---
+++ huffman/src/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    /*
+    below are the steps describing the functionalities 
+    in order to get this assignment done
+    
+    1. receive string, encode it using the algorithm
+    2. serialize the tree used
+    3. decode from serialized tree and code
+    4. interactions from terminal (save enconding, reading from file, etc)
+    */
+
+    exit(EXIT_SUCCESS);
+}
