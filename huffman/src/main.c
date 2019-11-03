4923c2b46700f528c8b30b918e9c8134e32e3230
dias <daniel.dddl.pdf@gmail.com>
2019-11-03 17:05:43 -0300

    updating minimal implementation description and also adding research resource

--- huffman/src/main.c
+++ huffman/src/main.c
@@ -4,14 +4,22 @@
 int main()
 {
     /*
-    below are the steps describing the functionalities 
-    in order to get this assignment done
-    
-    1. receive string, encode it using the algorithm
-    2. serialize the tree used
-    3. decode from serialized tree and code
-    4. interactions from terminal (save enconding, reading from file, etc)
+    First step: receive any string
+    Calculate the frequency of all characters
+    Build, in memory, the huffman tree
+    Print out the frequency of every character and its huffman code
+    Encode informed string 
+    Decode string previously encoded
+
+    Second step: file management and stuff, later
     */
+
+   /**
+    * Algorithms using a min heap as a way to extract the nodes with
+    * the least frequency and build the tree from there:
+    * https://www.geeksforgeeks.org/huffman-coding-greedy-algo-3/
+    * 
+   */
 
     exit(EXIT_SUCCESS);
 }
